1acc799ed06227bc516fee8f8e7df484a54e1986
Gerson Aznarán <gersonec12@gmail.com>
2025-09-22 15:49:29 -0500

    feat: add update and delete handlers for countries, and update payload structures

--- src/handlers/country.rs
+++ src/handlers/country.rs
@@ -1,13 +1,21 @@
-use actix_web::{Responder, Scope, get, post, web};
+use actix_web::{Responder, Scope, delete, get, post, put, web};
 
 use crate::{AppState, producer};
 
-// ESTRUCTURA PARA EL PAYLOAD DE CREACIÓN DE PAÍS
+// ESTRUCTURA PARA EL PAYLOAD DE CREACIÓN DE PAÍS (campos obligatorios)
 #[derive(serde::Deserialize, serde::Serialize)]
 struct CreateCountryRequest {
     name: String,
     code: String,
     dial_code: String,
+}
+
+// ESTRUCTURA PARA EL PAYLOAD DE ACTUALIZACIÓN DE PAÍS (campos opcionales)
+#[derive(serde::Deserialize, serde::Serialize)]
+struct UpdateCountryRequest {
+    name: Option<String>,
+    code: Option<String>,
+    dial_code: Option<String>,
 }
 
 #[post("")]
@@ -31,8 +39,32 @@ async fn get_countries_handler(
     producer::publish_message(&_state, "findByCriteria", "{}".into()).await
 }
 
+#[put("/{id}")]
+async fn update_country_handler(
+    _state: web::Data<AppState>,
+    _id: web::Path<String>,
+    _payload: web::Json<UpdateCountryRequest>,
+) -> impl Responder {
+    producer::publish_message(
+        &_state,
+        "updateCountry",
+        serde_json::json!({"id": _id.into_inner(), "updateCountryDto":_payload}),
+    )
+    .await
+}
+
+#[delete("/{id}")]
+async fn delete_country_handler(
+    _state: web::Data<AppState>,
+    _id: web::Path<String>,
+) -> impl Responder {
+    producer::publish_message(&_state, "removeCountry", _id.into_inner().into()).await
+}
+
 pub fn countries_scope() -> Scope {
     web::scope("/countries")
         .service(get_countries_handler)
         .service(create_country_handler)
+        .service(update_country_handler)
+        .service(delete_country_handler)
 }
